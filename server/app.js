aa074e56c11d996c315a286da489dc0a35401500
Ahamed Fahmi <it21037306@my.sliit.lk>
2024-09-14 21:08:13 +0530

    OAuth - Testing Final
    Signed-off-by: Ahamed Fahmi <it21037306@my.sliit.lk>

--- server/app.js
+++ server/app.js
@@ -7,56 +7,98 @@ import membershipRouter from "./routes/Membership.route.js";
 import trainerRouter from "./routes/Trainer.route.js";
 import trainerAppRouter from "./routes/TrainerApp.route.js";
 import reviewRouter from "./routes/Review.route.js";
-import productRouter from './routes/Product.route.js'
+import productRouter from "./routes/Product.route.js";
 import cartRouter from "./routes/Cart.route.js";
 import sessionRouter from "./routes/Session.route.js";
 import attendanceRouter from "./routes/Attendance.route.js";
 import paymentRouter from "./routes/Payment.route.js";
-import financeRouter from "./routes/Finance.route.js"
+import financeRouter from "./routes/Finance.route.js";
 import uploadImage from "./uploadImage.js";
+import session from "express-session";
+import router from "./OAuth.js";
+import passport from "passport";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3030",
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  })
+);
+
 app.use(express.json({ limit: "20mb" }));
 
-app.get('/', (req, res) => {
-    res.send('Server is Running! 🚀');
-})
+app.get("/", (req, res) => {
+  res.send("Server is Running! 🚀");
+});
+
+// Configure session middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, // Set to true if using HTTPS
+  })
+);
+
+// Initialize Passport and restore authentication state, if any, from the session
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express route for initiating Google OAuth2 authentication
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+// Express route for handling the callback from Google OAuth2
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "http://localhost:3030/home",
+    failureRedirect: "http://localhost:3030/login",
+  })
+);
 
 //User and Membership Management
-app.use('/user', userRouter);
-app.use('/admin', adminRouter);
-app.use('/membership', membershipRouter);
+app.use("/user", userRouter);
+app.use("/admin", adminRouter);
+app.use("/membership", membershipRouter);
+app.use("/auth", router);
 
 //Trainer Management
-app.use('/trainer', trainerRouter);
-app.use('/trainer/applicant', trainerAppRouter)
+app.use("/trainer", trainerRouter);
+app.use("/trainer/applicant", trainerAppRouter);
 
 //Customer Relationship Management
-app.use('/review', reviewRouter);
+app.use("/review", reviewRouter);
 
 //Product Management
-app.use('/product', productRouter);
-app.use('/cart', cartRouter);
+app.use("/product", productRouter);
+app.use("/cart", cartRouter);
 
 //Personal Training Management
-app.use('/session', sessionRouter);
-app.use('/attendance', attendanceRouter);
+app.use("/session", sessionRouter);
+app.use("/attendance", attendanceRouter);
 
 //Payment Management
-app.use('/payment', paymentRouter);
+app.use("/payment", paymentRouter);
 
 //Finance Management
-app.use('/finance', financeRouter);
+app.use("/finance", financeRouter);
 
 //Upload Image
 app.post("/uploadImage", (req, res) => {
-    uploadImage(req.body.image).then((url) => {
-        res.send(url)
-    }).catch((err) => {
-        res.status(500).send(err)
+  uploadImage(req.body.image)
+    .then((url) => {
+      res.send(url);
     })
-})
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
 
 export default app;
